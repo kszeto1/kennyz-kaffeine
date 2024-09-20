7e4458df8de774aed23adacdbd1ef971bc6270a7
kszeto1 <kenny.szeto1@gmail.com>
2024-09-19 22:57:12 -0700

    Add more padding to menu items and subheadings

--- app/products/FilteredProductList.tsx
+++ app/products/FilteredProductList.tsx
@@ -34,14 +34,14 @@ export default async function FilteredProductList({
     <div className="container mx-auto px-4 min-h-screen">
       <div className="flex flex-wrap -mx-4">
         {filteredProducts.map((product: Product) => (
-          <div key={product.id} className="w-full">
+          <div key={product.id} className="w-full pt-10">
             {product.items.map((item) => (
               <div key={item.subCategory} className="mb-8">
-                <h3 className="text-xl font-bold mb-4">{item.subCategory}</h3>
+                <h3 className="text-3xl font-bold mb-4 py-4">{item.subCategory}</h3>
                 <div className="flex flex-wrap -mx-2">
                   {item.items.map((subItem) => (
-                    <div key={subItem.name} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
-                      <div className="shadow-md p-4 h-full flex flex-col bg-[#ebe8de] rounded-lg">
+                    <div key={subItem.name} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 px-2 mb-4 mr-3">
+                      <div className="shadow-md p-10 h-full flex flex-col bg-[#ebe8de] rounded-lg">
                         <h4 className="text-lg font-bold mb-2">{subItem.name}</h4>
                         <p className="mb-4 flex-grow">{subItem.description}</p>
                         <img src={subItem.image} alt={subItem.name} className="w-full h-48 object-cover rounded" />
