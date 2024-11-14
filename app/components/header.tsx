d1dd3089175d269a3e860e6c69100ec811b95ec9
kszeto1 <kenny.szeto1@gmail.com>
2024-11-13 16:14:22 -0800

    Adjust shopping cart svg size

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -65,8 +65,8 @@ export default function Header() {
           <Link href="/" scroll={false}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="36px"
-              height="36px"
+              width="28px"
+              height="28px"
               viewBox="0 0 26 26"
               fill="none"
               // class="spree-icon"
