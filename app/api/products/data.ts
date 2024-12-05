be09628e1c97aa40fa9b975ebebdeec21aa83141
kszeto1 <kenny.szeto1@gmail.com>
2024-12-05 15:32:50 -0800

    Change revalidation of products data

--- app/api/products/data.ts
+++ app/api/products/data.ts
@@ -1,6 +1,9 @@
 export async function fetchProducts() {
     const res = await fetch('http://localhost:3000/api/products', {
-        cache: 'no-store'
+        cache: 'no-store',
+        next: {
+            revalidate: 3600,
+        }
     });
     if (!res.ok) {
         throw new Error('Failed to fetch productss');
